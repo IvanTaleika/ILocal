efa4680c132bd8d428e8b4e4251d0ecfe877156b
chizh <ivan.forever91@gmail.com>
2019-03-23 15:39:47 +0300

    add test files

---
+++ src/main/java/ILocal/TheMcutiApplication.java
@@ -0,0 +1,12 @@
+package ILocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheMcutiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheMcutiApplication.class, args);
+	}
+}
